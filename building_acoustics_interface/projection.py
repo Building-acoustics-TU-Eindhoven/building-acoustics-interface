549d137e8f0841b92417c4b51e52f6b6937f98f7
siperinio <104511201+siperinio@users.noreply.github.com>
2023-12-10 15:21:05 +0100

    created a projection class
    this class represents the projection matrix calculation to go to clip space.

---
+++ building_acoustics_interface/projection.py
@@ -0,0 +1,22 @@
+import math
+import numpy as np
+
+class Projection:
+    def __init__(self, render):
+        NEAR = render.camera.near_plane
+        FAR = render.camera.far_plane
+        RIGHT = math.tan(render.camera.h_fov / 2)
+        LEFT = -RIGHT
+        TOP = math.tan(render.camera.v_fov / 2)
+        BOTTOM = -TOP
+
+        m00 = 2 / (RIGHT - LEFT)
+        m11 = 2 / (TOP - BOTTOM)
+        m22 = (FAR + NEAR) / (FAR - NEAR)
+        m32 = -2 * NEAR * FAR / (FAR - NEAR)
+        self.projection_matrix = np.array([
+            [m00, 0, 0, 0],
+            [0, m11, 0, 0],
+            [0, 0, m22, 0],
+            [0, 0, m32, 0]
+        ])
