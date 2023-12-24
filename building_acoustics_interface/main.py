0b4708f02e90b0cd8471e251f45d5337ee7e45ba
siperinio <104511201+siperinio@users.noreply.github.com>
2023-12-24 19:50:31 +0100

    added frames for the render
    added the ability to embed a pygame window  in a tkinter window.

--- building_acoustics_interface/main.py
+++ building_acoustics_interface/main.py
@@ -2,6 +2,9 @@ from tkinter import *
 from toolbar import *
 from graphDisplay import *
 from resultsDisplay import *
+from renderer import *
+import os
+import platform
 
 # root window
 root = Tk()
@@ -11,7 +14,23 @@ screenWidth = root.winfo_screenwidth()
 root.geometry("{}x{}+0+0".format(screenWidth, screenHeight))
 root.state('zoomed')
 root.resizable(False, False)
+
 toolbar = Toolbar(root)
 graph_display = graphDisplay(root)
 results_display = resultsDisplay(root)
+pygame_frame = Frame(root, width=750, height=750, highlightbackground='#595959', highlightthickness=2)
+embed = Frame(pygame_frame, width=748, height=748,)
+pygame_frame.pack(side="left")
+embed.pack()
+
+#tihs embeds the pygame window
+os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
+sytem = platform.system()
+if sytem == "windows":
+    os.environ['SDL_VIDEODRIVER'] == 'windib'
+elif sytem == "Linux":
+    os.environ['SDL_VIDEODRIVER'] = 'x11'
+
+renderer = SoftwareRender()
+root.update_idletasks()
 root.mainloop()
