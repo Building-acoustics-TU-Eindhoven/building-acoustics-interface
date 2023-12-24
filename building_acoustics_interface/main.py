23a414c23ed6f9dcb1a1b0c4d019378a04308aba
siperinio <104511201+siperinio@users.noreply.github.com>
2023-12-24 19:57:18 +0100

    created a class for the main window
    creation of a class for the main window.

--- building_acoustics_interface/main.py
+++ building_acoustics_interface/main.py
@@ -1,36 +1,44 @@
-from tkinter import *
+import tkinter as tk
 from toolbar import *
 from graphDisplay import *
 from resultsDisplay import *
 from renderer import *
 import os
 import platform
+from object_3d import *
+from camera import *
+from projection import *
+import pygame as pg
 
 # root window
-root = Tk()
-root.title('Acoustic UI')
-screenHeight = root.winfo_screenheight()
-screenWidth = root.winfo_screenwidth()
-root.geometry("{}x{}+0+0".format(screenWidth, screenHeight))
-root.state('zoomed')
-root.resizable(False, False)
+class window():
+    def __init__(self):
+        #main window
+        self.root = tk.Tk() 
+        self.root.title("Acoustic UI")
+        self.root.configure(background='#9b9b9b')
+        screenHeight = self.root.winfo_screenheight()
+        screenWidth = self.root.winfo_screenwidth()
+        self.root.geometry("{}x{}+0+0".format(screenWidth, screenHeight))
+        self.root.state('zoomed')
+        self.root.resizable(False, False)
 
-toolbar = Toolbar(root)
-graph_display = graphDisplay(root)
-results_display = resultsDisplay(root)
-pygame_frame = Frame(root, width=750, height=750, highlightbackground='#595959', highlightthickness=2)
-embed = Frame(pygame_frame, width=748, height=748,)
-pygame_frame.pack(side="left")
-embed.pack()
+        #sub windows
+        self.toolbar = Toolbar(self.root)
+        self.graph_display = graphDisplay(self.root)
+        self.results_display = resultsDisplay(self.root)
+        self.renderer = Frame(self.root, width=750, height=750, highlightbackground='#595959', highlightthickness=2)
+        self.embed = Frame(self.renderer, width=748, height=748,)
+        self.renderer.pack(side="left")
+        self.embed.pack()
 
-#tihs embeds the pygame window
-os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
-sytem = platform.system()
-if sytem == "windows":
-    os.environ['SDL_VIDEODRIVER'] == 'windib'
-elif sytem == "Linux":
-    os.environ['SDL_VIDEODRIVER'] = 'x11'
+        #this embeds the pygame window
+        os.environ['SDL_WINDOWID'] = str(self.embed.winfo_id())
+        sytem = platform.system()
+        if sytem == "windows":
+            os.environ['SDL_VIDEODRIVER'] == 'windib'
+        elif sytem == "Linux":
+            os.environ['SDL_VIDEODRIVER'] = 'x11'
 
-renderer = SoftwareRender()
-root.update_idletasks()
-root.mainloop()
+        self.root.update_idletasks()
+        self.root.mainloop()
