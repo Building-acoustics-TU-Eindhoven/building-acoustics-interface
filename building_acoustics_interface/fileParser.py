de2a40527555a9b30242f604bfcdaee9c5dd4c82
siperinio <104511201+siperinio@users.noreply.github.com>
2023-12-30 18:25:20 +0100

    able to load msh 4.1
    now able to load msh files of version 4.1

--- building_acoustics_interface/fileParser.py
+++ building_acoustics_interface/fileParser.py
@@ -19,6 +19,43 @@ class FileParser:
             print("no good file")
             self.object = Object3D(self.render, [], [])
     
+    # format for this version of mesh files can be found here:
+    # https://gmshparser.readthedocs.io/en/latest/mesh_formats.html
+    # this is for the different type of shapes (third number per shape index):
+    # https://www.manpagez.com/info/gmsh/gmsh-2.2.6/gmsh_63.php#SEC63
+    def parse_msh_4_1(self):
+        vertices, faces = [], []
+        nodes_section = False
+        faces_section = False
+        select_triangles = False
+        counter_faces = 0
+        for line in self.file.split('\n'):
+            if (line == '$Nodes'):
+                nodes_section = True
+            elif (line == '$EndNodes'):
+                nodes_section = False
+            elif (line == '$Elements'):
+                faces_section = True
+            elif (line == '$EndElements'):
+                faces_section = False
+            elif (line.startswith("2 1 ") and faces_section and len(line.split()) == 4): #2d shapes start here
+                select_triangles = True 
+            elif (line.startswith("3 1 ") and select_triangles and len(line.split()) == 4): #stop ones we see 3d shapes
+                select_triangles = False
+            elif (nodes_section):
+                if line.strip():
+                    if (len(line.split()) == 3):
+                        values = [float(val) for val in line.split() + [1]]
+                        vertices.append(values)
+            elif (select_triangles):
+                if line.strip():
+                    if (line.split()[0] == '2' and line.split()[2] == '2' and counter_faces == 0):
+                        counter_faces = line.split()[3]
+                    if (line.split()[0] != '2'):
+                        values = [int(val) - 1 for val in line.split()[1:]]
+                        faces.append(values)
+        self.object = Object3D(self.render, vertices, faces)
+    
     def parse_obj(self):
         vertices, faces = [], []
         for line in self.file.split('\n'):
@@ -39,16 +76,14 @@ class FileParser:
             elif (line == '$EndNodes'):
                 nodes_section = False
             elif (line == '$Elements'):
-                print("i get here")
                 faces_section = True
             elif (line == '$EndElements'):
                 faces_section = False
 
             elif (nodes_section):
                 if line.strip():
-                    check = line.split()[1:]
-                    if (len(check) != 0):
-                        values = [float(val) for val in check + [1]]
+                    if (len(line.split()[1:]) != 0):
+                        values = [float(val) for val in line.split()[1:] + [1]]
                         vertices.append(values)
             elif (faces_section):
                 if line.strip():
